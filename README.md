fe9cf52edfe3d4ee0fc6bfb208bb37aba4b0667d
Marco Gruhl <marco.gruhl@xecuris.com>
2023-04-21 13:36:47 +0200

    added usage to readme added console sample

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ Tested with fonts from [rpi-rgb-led-matrix](https://github.com/hzeller/rpi-rgb-l
 I needed this for my Matrix Simulator:
 
 ![Matrix Simulator](https://github.com/marcogruhl/BdfFontParser/raw/main/MatrixSimulator.png "Matrix Simulator")
+
+## Usage
+
+```C#
+var font = new BdfFont("fonts/7x13B.bdf");
+
+foreach (var c in "Hello World".ToCharArray())
+{
+    var charArray = font[c];
+
+    var y = yStart;
+
+    foreach (var b in charArray.Bitmap.Reverse())
+    {
+        var x = xStart;
+
+        foreach (var bin in Convert.ToString(b, 2).PadLeft(8, '0').ToCharArray())
+        {
+```
---
+++ Samples/Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using BdfFontParser;
+using System.Text;
+
+var font = new BdfFont("fonts/7x13B.bdf");
+
+// draw line by line
+for (int line = 2; line < font.BoundingBox.Y; line++)
+{
+    var sb = new StringBuilder();
+
+    foreach (var c in "Hello World".ToCharArray())
+    {
+        var charArray = font[c];
+        var x = 0;
+        
+        // foreach (var b in charArray.Bitmap)
+        // {
+
+        byte b = (byte)charArray.Bitmap.GetValue(line);
+
+            foreach (var bin in Convert.ToString(b, 2).PadLeft(8, '0').ToCharArray())
+            {
+                // if (x != line)
+                //     continue;
+
+                sb.Append(bin == '1' ? '#' : ' ');
+            }
+                    x++;
+
+            // }
+        // }
+
+    } 
+
+    Console.WriteLine(sb.ToString());
+
+}
