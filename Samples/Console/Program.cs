dfa9460c5847ee141b34574e304ef8a48bffd3de
Marco Gruhl <marco.gruhl@xecuris.com>
2023-04-21 13:39:05 +0200

    cleaned sample console

--- Samples/Console/Program.cs
+++ Samples/Console/Program.cs
@@ -8,30 +8,21 @@ for (int line = 2; line < font.BoundingBox.Y; line++)
 {
     var sb = new StringBuilder();
 
+    // iterate through every character
     foreach (var c in "Hello World".ToCharArray())
     {
-        var charArray = font[c];
-        var x = 0;
-        
-        // foreach (var b in charArray.Bitmap)
-        // {
+        // get the bitmap from the character
+        var bitmap = font[c].Bitmap;
 
-        byte b = (byte)charArray.Bitmap.GetValue(line);
+        // get the byte for the specific line
+        var b = (byte)bitmap.GetValue(line);
 
-            foreach (var bin in Convert.ToString(b, 2).PadLeft(8, '0').ToCharArray())
-            {
-                // if (x != line)
-                //     continue;
-
-                sb.Append(bin == '1' ? '#' : ' ');
-            }
-                    x++;
-
-            // }
-        // }
-
-    } 
+        // iterate through every bit
+        foreach (var bin in Convert.ToString(b, 2).PadLeft(8, '0').ToCharArray())
+        {
+            sb.Append(bin == '1' ? '#' : ' ');
+        }
+    }
 
     Console.WriteLine(sb.ToString());
-
 }
